561b0b2dc9c6705b26e603b643051ffa2255f65e
GabrielaSenderski <gabrielasenderski.ifc@gmail.com>
2025-09-01 11:33:26 -0300

    FEAT: Finalização do código de backend #26

--- BackEnd1/config/urls.py
+++ BackEnd1/config/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from routerifc.views import EmpresaViewSet
+from routerifc.views import EmpresaViewSet, MotoristaViewSet, VeiculoViewSet, RotasViewSet, EnderecoViewSet, PassageiroViewSet
 
 router = DefaultRouter()
 router.register(r'empresas', EmpresaViewSet)
+router.register(r'motoristas', MotoristaViewSet)
+router.register(r'veiculos', VeiculoViewSet)
+router.register(r'rotas', RotasViewSet)
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'passageiros', PassageiroViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- BackEnd1/routerifc/views.py
+++ BackEnd1/routerifc/views.py
@@ -1,7 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Empresa
-from .serializers import EmpresaSerializer
+from .models import Empresa, Motorista, Veiculo, Rotas, Endereco, Passageiro
+from .serializers import EmpresaSerializer, MotoristaSerializer, VeiculoSerializer, RotasSerializer, EnderecoSerializer, PassageiroSerializer
 
 class EmpresaViewSet(ModelViewSet):
     queryset = Empresa.objects.all()
     serializer_class = EmpresaSerializer
+
+class MotoristaViewSet(ModelViewSet):
+    queryset = Motorista.objects.all()
+    serializer_class = MotoristaSerializer
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
+
+class RotaViewSet(ModelViewSet):
+    queryset = Rota.objects.all()
+    serializer_class = RotaSerializer
+
+class EnderecoViewSet(ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
+
+class PassageiroViewSet(ModelViewSet):
+    queryset = Passageiro.objects.all()
+    serializer_class = PassageiroSerializer
